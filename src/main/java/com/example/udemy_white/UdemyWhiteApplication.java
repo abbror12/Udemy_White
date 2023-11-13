2337f8e92d6e9d579dd86aba5e7b42622a317cf0
Abror <yoldashevabror182@gmail.com>
2023-11-13 17:23:08 +0500

    initial commit

---
+++ src/main/java/com/example/udemy_white/UdemyWhiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemy_white;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyWhiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyWhiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/udemy_white/UdemyWhiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udemy_white;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyWhiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
